effb234b8bd869e8bd88a8c1b2574c6b54c48f8e
Artur Kalil <arturkcorr3a@gmail.com>
2023-11-13 21:18:40 -0300

    Atualização nos métodos construtor e toString da classe Local

--- Projeto/src/Local.java
+++ Projeto/src/Local.java
@@ -2,7 +2,19 @@ public class Local {
 
 	private String cidade;
 	private int codigo;
-	private static int codigoAUX;
+	private static int codigoAUX = 1;
 	private String nome;
+
+	public Local(String cidade, String nome) {
+		this.cidade = cidade;
+		this.nome = nome;
+		this.codigo = codigoAUX;
+		codigoAUX++;
+	}
+
+	@Override
+	public String toString() {
+		return "Cidade: " + cidade + "; Codigo: " + codigo + "; Nome: " + nome + ";\n";
+	}
 
 }
