9424201a7bde886a597bcf8be30af2a81927ad5e
Geferson <gefersonrodriguesrocha@gmail.com>
2023-11-15 16:18:12 -0300

    Atualização da classe cliente

--- Projeto/src/Cliente.java
+++ Projeto/src/Cliente.java
@@ -4,4 +4,32 @@ public class Cliente {
 	private static int codigoAUX;
 	private String nome;
 	private String telefone;
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public static int getCodigoAUX() {
+		return codigoAUX;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	@Override
+	public String toString() {
+		return "Cliente [codigo=" + codigo + ", nome=" + nome + ", telefone=" + telefone + "]";
+	}
+
+	
 }
