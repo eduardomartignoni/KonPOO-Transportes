d8d41edff60e614768b81a168cca8244eeb19543
Artur Kalil <arturkcorr3a@gmail.com>
2023-11-14 23:39:04 -0300

    Finalizada classe enum STatus

--- Projeto/src/Status.java
+++ Projeto/src/Status.java
@@ -1,7 +1,17 @@
 public enum Status {
 
-	CANCELADA, FINALIZADA, LOCADA, PENDENTE;
+	CANCELADA(-1, "Cancelada"), FINALIZADA(0, "Finalizada"), 
+	LOCADA(1, "Locada"), PENDENTE(2, "Pendente");
 	
 	private int id;
-	
+	private String nome;
+
+	private Status(int id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
 }
